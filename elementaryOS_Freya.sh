b5cdc01dcaeaa4da243c1538148d4b48ad0f9ed5
mthjn <jana.mathauserova@ubk.cz>
2015-06-10 12:22:53 +0200

    screen s

--- elementaryOS_Freya.sh
+++ elementaryOS_Freya.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
 sudo apt-get update
 sudo apt-get install elementary-tweaks
 
-# startup application
+# startup applications
 sudo gnome-session-properties
 
 # get cairo dock instead of plank
@@ -16,6 +16,20 @@ dconf-editor
 #remove plank from the array and type in 'cairo-dock'
 pkill plank
 
+# screenlets for desktop
+sudo add-apt-repository ppa:screenlets/ppa
+sudo apt-get update
+sudo apt-get install screenlets
+
+## SysmonitorScreenlet ClearCalendarScreenlet WidescapeWeatherScreenlet eventCalScreenlet
+## CircleClockScreenlet FeedReaderPlusScreenlet NowPlayingScreenlet
+
+# get web browsers
+sudo apt-get purge midori-granite
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
 
 # @link http://www.neowin.net/forum/topic/1180735-guide-to-make-elementary-os-look-like-xenzen-configuration/
 # @link http://lifehacker.com/the-elementary-desktop-1304577213
