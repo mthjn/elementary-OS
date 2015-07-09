2eaca25b7295a8aee4d38d86dd78ae6524517a85
mthjn <jana.mathauserova@ubk.cz>
2015-07-09 12:30:39 +0200

    get skype audio on linux

--- Ubuntu_elementary_SkypeAudio.sh
+++ Ubuntu_elementary_SkypeAudio.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# on clean install as root
+apt-get install pulseaudio
+apt-get install pavucontrol -y
+wget -O skype-install.deb http://www.skype.com/go/getskype-linux-deb
+dpkg -i skype-install.deb
+# unmet dependencies
+apt-get -f install -y
+dpkg -i skype-install.deb
+
+apt-get update
+apt-get upgrade -y
