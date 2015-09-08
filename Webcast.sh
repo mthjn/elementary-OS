e08dc42becab26dada8eecafdc820a37aa83de42
I won't tell <mthjn@users.noreply.github.com>
2015-09-08 13:04:49 +0200

    Update Webcast.sh

--- Webcast.sh
+++ Webcast.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+#java tomcat container https://help.ubuntu.com/lts/serverguide/tomcat.html
+sudo apt-get install tomcat7
+
 # cisco webex missing libs
-sudo apt install libpangoxft-1.0-0:i386 libxv1:i386 libpangox-1.0-0:i386
+sudo apt install libpangoxft-1.0-0:i386 libxv1:i386 libpangox-1.0-0:i386 -y
+
+
